cc7f9955e40d1a55453befe3e9d63c00edc09fbb
yuluhuang <504367857@qq.com>
2014-09-22 21:24:58 +0800

    aa

--- models/setting.js
+++ models/setting.js
@@ -3,7 +3,7 @@ module.exports = {
   //db: "mongodb://fc19zAhk:ng08AuXlIWe7@10.0.31.20:27017/04367857m_mongo_23fi32ue",//'04367857m_mongo_23fi32ue',
   //host: '10.0.31.20',
   db:"04367857m_mongo_23fi32ue",
-  host: 'localhost',
+  host: '10.0.31.20',
   url:'mongodb://fc19zAhk:ng08AuXlIWe7@10.0.31.20:27017/04367857m_mongo_23fi32ue',
 
 };
