536fc39b79cd5b329e68912455b09f98b8ddcdf9
yuluhuang <504367857@qq.com>
2014-09-22 18:59:15 +0800

    aa

--- models/db.js
+++ models/db.js
@@ -2,5 +2,5 @@ var settings = require('./setting');
 var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
-
+//var MONGOHQ_URL="mongodb://fc19zAhk:ng08AuXlIWe7@server.mongohq.com:27017/04367857m_mongo_23fi32ue"//这个url就是mongohq给的Connection string
 module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT,{});
