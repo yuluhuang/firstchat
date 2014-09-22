e532d41b662b0232dcb4501437ba1b20ac14e9d4
yuluhuang <504367857@qq.com>
2014-09-22 19:03:17 +0800

    a

--- models/setting.js
+++ models/setting.js
@@ -1,6 +1,6 @@
 module.exports = {
   cookieSecret: 'nodechat',
   db: '04367857m_mongo_23fi32ue',
-  host: 'fc19zAhk:ng08AuXlIWe7@10.0.31.21',
+  host: 'fc19zAhk:ng08AuXlIWe7@localhost',
   //host: 'localhost',
 };
