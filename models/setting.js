e836cd178a3175ba90a705ee4491014ddc3868a7
yuluhuang <504367857@qq.com>
2014-09-23 08:42:16 +0800

    aa

--- models/setting.js
+++ models/setting.js
@@ -1,9 +1,9 @@
 module.exports = {
   cookieSecret: 'nodechat',
   //db: "mongodb://fc19zAhk:ng08AuXlIWe7@10.0.31.20:27017/04367857m_mongo_23fi32ue",//'04367857m_mongo_23fi32ue',
-  //host: '10.0.31.20',
-  db:"04367857m_mongo_23fi32ue",
-  host: '10.0.31.20:27017',
-  url:'mongodb://fc19zAhk:ng08AuXlIWe7@10.0.31.20:27017/04367857m_mongo_23fi32ue',
+  host: '10.0.31.20',
+  db:"fc19zAhk:ng08AuXlIWe7@04367857m_mongo_23fi32ue",
+  //host: 'localhost',
+  url:process.env.MONGOLAB_URI ||process.env.MONGOHQ_URL ||'mongodb://fc19zAhk:ng08AuXlIWe7@10.0.31.20:27017/04367857m_mongo_23fi32ue',
 
 };
