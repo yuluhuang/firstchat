1b392afe8095640348c50c9e4f83a931fbbb5043
yuluhuang <504367857@qq.com>
2014-10-08 09:39:57 +0800

    mongodb

--- models/setting.js
+++ models/setting.js
@@ -1,17 +1,10 @@
-//mongo ds039850.mongolab.com:39850/nodeapp -u admin -p admin
-// module.exports = {
-//   cookieSecret: 'nodechat',
-//   db:'04367857m_mongo_0qw7b8g8',
-//   port:'27017',
-//   host: 'localhost',
-//   username:'',
-//   password:''
-// };
+
 module.exports = {
-  cookieSecret: 'nodechat',
-  db:'nodeapp',
-  port:'39850',
-  host: 'ds039850.mongolab.com',
-  username:'admin',
-  password:'admin'
+   cookieSecret: 'nodechat',
+   db:'04367857m_mongo_0qw7b8g8',
+   port:'27017',
+   host: 'localhost',
+   username:'',
+   password:''
 };
+
